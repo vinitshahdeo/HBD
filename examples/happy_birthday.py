0ced87ae5b1de71cff8a028a2723007a890e67a3
saltgen <sdnirvana94@gmail.com>
2019-10-12 08:37:39 +0530

    Added a happy birthday script in Python

---
+++ examples/happy_birthday.py
@@ -0,0 +1,16 @@
+import webbrowser, time
+
+
+def hdb(name):
+    return (
+        "Many many happy returns for the day "
+        + name
+        + "! Stay tuned, there's a surprise for you :-D"
+    )
+
+
+bday_name = input("Hey there sugar, what's your name: ")
+print(hdb(bday_name))
+time.sleep(3.5)
+webbrowser.open("https://www.youtube.com/watch?v=NCzgzO1y7yQ")
+
