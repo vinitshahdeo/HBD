990e0c7a1a683dd83fc67afa1368a09ef6674a32
Saurav Jaiswal <42805104+Saurav80@users.noreply.github.com>
2019-10-14 23:23:12 +0530

    My First commit
    /* I write HAPPY BIRTHDAY in C programming language */

---
+++ examples/saurav80.c
@@ -0,0 +1,121 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <string.h>
+int main(){
+	char n[10],ch = ';';
+	int i,a,b;
+ 	for(i=0; i<34; i++)
+ 	{
+ 		if (i==4||i==6||i==9||i==10||i==13||i==14||
+ 			i==15||i==17||i==18||i==19||i==21||i==23)
+ 			printf("%c",ch) ;
+ 		else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+
+ 	for(i=0; i<34; i++)
+ 	{
+ 		if(i==4||i==6||i==8||i==11||i==13||i==15||
+			i==17||i==19||i==21||i==23)
+ 			printf("%c",ch) ;
+	 	else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+
+ 	for (i=0; i<34; i++)
+ 	{
+ 		if (i==4||i==5||i==6||i==8||i==9||i==10||i==11||
+ 			i==13||i==14||i==15||i==17||i==18||i==19||
+ 			i==21||i==22||i==23)
+			printf("%c",ch) ;
+	 	else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+
+ 	for (i=0; i<34; i++)
+ 	{
+ 		if (i==4||i==6||i==8||i==11||i==13||i==17||i==22)
+ 			printf("%c",ch) ;
+	 	else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+
+ 	for (int i=0; i<34; i++)
+ 	{
+ 		if (i==4||i==6||i==8||i==11||i==13||i==17||i==22)
+ 			printf("%c",ch) ;
+	 	else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+	printf("\n");
+
+	for (i=0; i<34; i++)
+ 	{
+ 		if (i==1||i==2||i==3||i==5||i==6||i==7||i==9||
+ 			i==10||i==13||i==14||i==15||i==17||i==19||
+ 			i==21||i==22||i==26||i==27||i==30||i==32)
+ 			printf("%c",ch) ;
+	 	else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+
+ 	for(i=0; i<34; i++)
+ 	{
+ 		if (i==1||i==3||i==6||i==9||i==11||
+ 			i==14||i==17||i==19||i==21||i==23||
+ 			i==25||i==28||i==30||i==32)
+ 			printf("%c",ch) ;
+	 	else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+
+ 	for (i=0; i<34; i++)
+ 	{
+ 		if (i==1||i==2||i==3||i==6||i==9||i==10||
+ 			i==14||i==17||i==18||i==19||i==21||
+ 			i==23||i==25||i==26||i==27||i==28||i==30||
+			i==31||i==32)
+ 			printf("%c",ch) ;
+	 	else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+
+ 	for (i=0; i<34; i++)
+ 	{
+ 		if (i==1||i==3||i==6||i==9||i==11||
+ 			i==14||i==17||i==19||i==21||
+ 			i==23||i==25||i==28||i==31)
+ 			printf("%c",ch) ;
+	 	else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+
+ 	for (i=0; i<34; i++)
+ 	{
+ 		if (i==1||i==2||i==3||i==5||i==6||i==7||
+ 			i==9||i==11||i==14||i==17||i==19||
+ 			i==22||i==21||i==25||i==28||i==31)
+ 			printf("%c",ch) ;
+	 	else
+			printf(" ") ;
+ 	}
+ 	printf("\n");
+	printf("\n");
+
+
+
+
+
+
+	return 0;
+
+}
