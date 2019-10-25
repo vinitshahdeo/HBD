81342da3588970f44313748578aae134946c9e7b
Nicole Yson <nicole.yson@vacasa.com>
2019-10-24 23:10:11 -0700

    adds goplayground link

--- examples/happy_birthday.go
+++ examples/happy_birthday.go
@@ -5,6 +5,7 @@ import (
 )
 
 // Run go build happy_birthday.go && ./happy_birthday
+// https://play.golang.org/p/lf2mLYMvCcR
 func main() {
 	birthdayGreeting := figure.NewFigure("Happy Birthday!!!!!", "", true)
 	birthdayGreeting.Print()
