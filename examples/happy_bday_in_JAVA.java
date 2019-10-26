0e48681f7de249cc27d81855e1fd278a298038fd
Nirmal patel <nmpatel347@gmail.com>
2019-10-26 13:43:40 +0530

    Delete happy_bday_in_JAVA.java

--- examples/happy_bday_in_JAVA.java
+++
@@ -1,6 +0,0 @@
-public class happy_bday_in_JAVA
-{
-    public static void main(String[] args) {
-        System.out.println("Happy Birthday Dear Friend! I hate coding in this language, but  still doing for you only because you deserve a birthday wish. Cake for all!");
-    }
-}
