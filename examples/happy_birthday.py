bbbb5e8b4540f1857f79bcae74ceb6d57d03748c
Michael <agentdonoso@gmail.com>
2019-10-30 20:24:44 -0400

    Happy Birthday + Cake

---
+++ examples/happy_birthday.py
@@ -0,0 +1,24 @@
+# Ambiguous Variables
+j=10
+n=j-1
+y=n
+u=1
+i=0
+
+# Create Cake
+while i<j:
+ i+=1
+ for z in range (n):
+  print('  ',end="")
+ for q in range (u):
+  print('||',end="")
+ print ("")
+ u+=2
+ n-=1
+ 
+# Print Happy Birthday!
+for k in range (y):
+ print ('  ',end="")
+print ("")
+print ("HAPPY BIRTHDAY")
+ 
