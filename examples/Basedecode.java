69a5c36c131f73ce575aedb80a16ea2be1695f78
meghnabajoria <50470784+meghnabajoria@users.noreply.github.com>
2020-10-01 02:16:14 +0530

    Update Basedecode.java

--- examples/Basedecode.java
+++ examples/Basedecode.java
@@ -4,9 +4,9 @@ public class Basedecode
 {
 	public static void main(String[] args)
 	{
-        String str="SGFwcHkgQmlydGhkYXkgISEgOik=";
-        Base64.Decoder decoder = Base64.getDecoder();
-        String dStr = new String(decoder.decode(str));  
-        System.out.println(dStr);  
+		String str="SGFwcHkgQmlydGhkYXkgISEgOik=";
+		Base64.Decoder decoder = Base64.getDecoder();
+		String dStr = new String(decoder.decode(str));  
+		System.out.println(dStr);  
 	}
 }
