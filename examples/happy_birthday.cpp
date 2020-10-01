d9461d8dfeb526680952a351a193661f96123d06
Louis Sardarescu <louisrd1337@gmail.com>
2020-10-01 10:24:13 +0300

    C++ happy birthday

--- examples/happy_birthday.cpp
+++ examples/happy_birthday.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 int main()
 {
-    cout<<"DRAWING BIRTHDAY CAKE USING C++\n"<<endl;
 cout<<"     *        *        *\n    ***      ***      ***"<<endl;
     for (int x=0; x<3; x++)
     {
@@ -13,7 +12,7 @@ cout<<"     *        *        *\n    ***      ***      ***"<<endl;
     cout<<"|==========================|"<<endl;
     for(int x=0; x<2; x++)
 {
-        cout<<"|      HAPPY BIRTHDAY      |\n|~~~~~~~~~~~~~~~~~~~~~~~~~~|"<<endl;
+        cout<<"|      HAPPY BIRTHDAY HAKTOBERFEST    |\n|~~~~~~~~~~~~~~~~~~~~~~~~~~|"<<endl;
 }
 cout<<"|__________________________|\n \n 🎶🎶🎶🎶🎶🎶🎶";
 }
