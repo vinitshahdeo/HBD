f20483f630257f423cdc7bd74291668829379c24
Adiv Harjadinata <adiv.dinata@gmail.com>
2020-10-01 13:10:54 +0700

    Added semicolon

--- examples/happy_birthday.js
+++ examples/happy_birthday.js
@@ -1,3 +1,3 @@
-var name = prompt("What's your name ?")
+var name = prompt("What's your name ?");
 
-alert("Happy Birthday " + name + " I hope your birthday is the happiest ")
+alert("Happy Birthday " + name + " I hope your birthday is the happiest "); 
