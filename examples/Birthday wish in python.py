789083e00834da8468808ff30511ece22e7e5ef4
Shobhit Aryan Lal <55244666+Shobhit-aryan@users.noreply.github.com>
2020-10-01 12:00:33 +0530

    Removed trailing spaces

--- examples/Birthday wish in python.py
+++ examples/Birthday wish in python.py
@@ -6,14 +6,14 @@ Created on Thu Oct  1 11:38:11 2020
 """
 
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 words="Happy Birthday God Bless Enjoy Fun Party Love Friend Smile Gifts Laugh Dance Foods Wishes Special Surprises Cake Celebrate Music"
-wordcloud = WordCloud(width = 800, height = 800, 
+wordcloud = WordCloud(width = 800, height = 800,
                 background_color ='white',
-                min_font_size = 5).generate(words) 
+                min_font_size = 5).generate(words)
 
-plt.figure(figsize = (8, 8), facecolor = None) 
-plt.imshow(wordcloud) 
-plt.axis("off") 
+plt.figure(figsize = (8, 8), facecolor = None)
+plt.imshow(wordcloud)
+plt.axis("off")
 plt.tight_layout(pad = 0)
 plt.show()
