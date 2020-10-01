697acd87a49770ba084db2e0ca4f4218dd2747a0
Kalpan Mukherjee <43297951+kalpanmukherjee@users.noreply.github.com>
2020-10-01 22:20:41 +0530

    Update happy_birthday.cpp

--- examples/happy_birthday.cpp
+++ examples/happy_birthday.cpp
@@ -3,16 +3,12 @@ using namespace std;
 void dfs(int i, int j, vector<vector<char>>& hbd, vector<vector<int>>& visited) {
     if(i<0 || j<0 || i>=hbd.size() || j>=hbd[0].size() || visited[i][j])
         return;
-    
     visited[i][j] = 1;
-    
     cout<<hbd[i][j]<<" ";
-    
     dfs(i+1, j, hbd, visited);
     dfs(i, j+1, hbd, visited);
     dfs(i, j-1, hbd, visited);
-    dfs(i-1, j, hbd, visited);
-    
+    dfs(i-1, j, hbd, visited); 
 }
 int main() { 
     vector<vector<char>> hbd{
@@ -21,10 +17,7 @@ int main() {
         {'P','H','T','R'},
         {'P','Y','B','I'}
     };
-    
     vector<vector<int>> visited(hbd.size(), vector<int>(hbd[0].size(), 0));
-    
     dfs(0, 0, hbd, visited);
-    
     return 0;
 }
