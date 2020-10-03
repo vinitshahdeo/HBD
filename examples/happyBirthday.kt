12381563170a0c3e8ac87489eaa3dae683b355de
Yothin homjan <yothin.indy.8@gmail.com>
2020-10-03 15:41:39 +0700

    Create happyBirthday.kt

---
+++ examples/happyBirthday.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println("Happy Birthday")
+    
+    // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    
+    // This prints an empty line.
+    println("")
+
+    println("You are already 5!")
+    println("5 is the very best age to celebrate!")
+}
