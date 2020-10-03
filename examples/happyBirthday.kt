07ecf42f25a3d02cf5e57d1d3a1e539987ae6cc5
Yothin homjan <yothin.indy.8@gmail.com>
2020-10-03 15:44:07 +0700

    Update happyBirthday.kt

--- examples/happyBirthday.kt
+++ examples/happyBirthday.kt
@@ -1,17 +1,12 @@
 fun main() {
     println("Happy Birthday")
-    
-    // Let's print a cake!
     println("   ,,,,,   ")
     println("   |||||   ")
     println(" =========")
     println("@@@@@@@@@@@")
     println("{~@~@~@~@~}")
     println("@@@@@@@@@@@")
-    
-    // This prints an empty line.
     println("")
-
     println("You are already 5!")
     println("5 is the very best age to celebrate!")
 }
