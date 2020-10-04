b93997a08b205b5c3106da84b15133fe62ddeb80
hemant404 <56862469+hemant404@users.noreply.github.com>
2020-10-04 14:42:23 +0530

    Added HBD in chsarp.

---
+++ examples/happy_birthday_csharp.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HappyBirthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Mguu~%Gnwymif~%?.";
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write((char)(message[i] - 5));
+            }
+
+        }
+
+    }
+}
