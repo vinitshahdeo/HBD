cb426382ae790822485389f5c41d0abc9f2ddccf
Geeky-Star <nimratkaur540@gmail.com>
2020-10-05 06:25:16 +0530

    Update hbd.py

--- hbd.py
+++ hbd.py
@@ -1,15 +1,8 @@
-#print happy birthday in reverse order then ask birthday person to reverse again
-
 s = "HappyBirthday"
 r = "MayYouLiveLong"
 h = "WewishYouHappyBirthday"
+print(s[::-1])
+print(r[::-1])
+print(h[::-1])
 
-print(s[::-1])
-print(r[::-1])
-print(h[::-1])
-print(s[::-1])
-print(r[::-1])
-print(h[::-1])
-print(s[::-1])
-print(r[::-1])
 
