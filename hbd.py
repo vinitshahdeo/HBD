cb53994381fe714dec66ef533547b39c62e6e16f
Geeky-star <nimratkaur540@gmail.com>
2020-10-05 06:19:14 +0530

    added two files html and python

---
+++ hbd.py
@@ -0,0 +1,15 @@
+#print happy birthday in reverse order then ask birthday person to reverse again
+
+s = "HappyBirthday"
+r = "MayYouLiveLong"
+h = "WewishYouHappyBirthday"
+
+print(s[::-1])
+print(r[::-1])
+print(h[::-1])
+print(s[::-1])
+print(r[::-1])
+print(h[::-1])
+print(s[::-1])
+print(r[::-1])
+
