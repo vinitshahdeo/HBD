6e31f667c0903b9959371317893bd380c5a59eaf
c0d1ng-Devta <63671163+c0d1ng-Devta@users.noreply.github.com>
2020-10-05 17:12:27 +0530

    Create Happy_birthday.py
    Used Caesar Cipher Technique

---
+++ examples/Happy_birthday.py
@@ -0,0 +1,14 @@
+text="OhwwfIpyaokhf"
+s=-7
+result = ""
+   # transverse the plain text
+for i in range(len(text)):
+    char = text[i]
+      # Encrypt uppercase characters in plain text
+      
+    if (char.isupper()):
+        result += chr((ord(char) + s-65) % 26 + 65)
+      # Encrypt lowercase characters in plain text
+    else:
+        result += chr((ord(char) + s - 97) % 26 + 97)
+print( result)
