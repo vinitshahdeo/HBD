642b3b5659ad6bcb1d79d9e0883a673dac78d818
NihalSuresh007 <72062734+NihalSuresh007@users.noreply.github.com>
2020-10-08 21:54:45 +0530

    Create happy-birthday.py

---
+++ examples/happy-birthday.py
@@ -0,0 +1,95 @@
+ch = '@'
+for i in range(1,(34+1)):    
+    if (i==5 or i==7 or i==10 or
+        i==11 or i==14 or i==15 or
+        i==16 or i==18 or i==19 or
+        i==20 or i==22 or i==24): 
+        print(ch,end="") 
+    else: 
+        print(" ", end="") 
+print() 
+for i in range(1,(34+1)):       
+    if(i==5 or i==7 or i==9 or
+       i==12 or i==14 or i==16
+        or i==18 or i==20 or
+       i==22 or i==24): 
+        print(ch,end="") 
+    else: 
+        print(" ", end="") 
+print() 
+for i in range(1,(34+1)):       
+    if (i==5 or i==6 or i==7 or
+        i==9 or i==10 or i==11 or i==12 or
+        i==14 or i==15 or i==16 or
+        i==18 or i==19 or i==20 or
+        i==22 or i==23 or i==24): 
+        print(ch,end="") 
+    else: 
+        print(" ", end="") 
+print() 
+for i in range(1,(34+1)):       
+    if (i==5 or i==7 or i==9 or i==12
+        or i==14 or i==18 or i==23): 
+        print(ch,end="") 
+    else: 
+        print(" ", end="") 
+print() 
+for i in range(1,(34+1)):       
+    if (i==5 or i==7 or i==9 or
+        i==12 or i==14 or i==18 or i==23): 
+        print(ch,end="") 
+    else: 
+        print(" ",end="") 
+print() 
+print() 
+print() 
+for i in range(1,(34+1)):           
+    if (i==2 or i==3 or i==4 or
+        i==6 or i==7 or i==8 or i==10 or
+        i==11 or i==14 or i==15 or
+        i==16 or i==18 or i==20 or
+        i==22 or i==23 or i==27 or
+        i==28 or i==31 or i==33): 
+        print(ch,end="") 
+    else: 
+        print(" ",end="") 
+print() 
+for i in range(1,(34+1)):       
+    if (i==2 or i==4 or i==7 or i==10 or i==12 or
+        i==15 or i==18 or i==20 or i==22 or i==24 or
+        i==26 or i==29 or i==31 or i==33): 
+        print(ch,end="") 
+    else: 
+        print(" ",end="") 
+print() 
+for i in range(1,(34+1)):       
+    if (i==2 or i==3 or i==4 or
+        i==7 or i==10 or i==11 or
+        i==15 or i==18 or i==19 or
+        i==20 or i==22 or
+        i==24 or i==26 or i==27 or
+        i==28 or i==29 or i==31
+         or i==32 or i==33): 
+        print(ch,end="") 
+    else: 
+        print(" ",end="") 
+print() 
+for i in range(1,(34+1)):       
+    if (i==2 or i==4 or i==7 or i==10 or i==12 or
+        i==15 or i==18 or i==20 or i==22 or
+        i==24 or i==26 or i==29 or i==32): 
+        print(ch,end="") 
+    else: 
+        print(" ", end="") 
+print() 
+for i in range(1,(34+1)): 
+    if (i==2 or i==3 or i==4 or
+        i==6 or i==7 or i==8 or
+        i==10 or i==12 or i==15 or
+        i==18 or i==20 or
+        i==23 or i==22 or i==26 or
+        i==29 or i==32): 
+        print(ch,end="") 
+    else: 
+        print(" ", end="") 
+print() 
