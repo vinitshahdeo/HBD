44a76a9fa52b66963974d191d8bf512348f31b47
jeremynac <jeremynac@localhost.localdomain>
2020-10-18 19:09:46 +0200

    added bash hbd

---
+++ examples/happy_birthday.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+crypted="31550 27463810"
+decrypt=(Y A B H R P T I D)
+zero="0"
+for i in {0..8}
+do
+    for ii in {0..60}
+    do
+        echo -ne "$(echo "$crypted" | sed "s/$i/$zero/g")\r"
+        if [[ $zero == "9" ]]; then
+            zero="0"
+        else
+            zero=$((zero + 1))
+        fi
+        sleep 0.01
+    done
+    echo -ne "$crypted\r"
+    crypted="$(echo "$crypted" | sed "s/${i}/${decrypt[i]}/g" | sed "s/*./\L&/g")"
+done
+echo "$crypted"
