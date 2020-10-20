c3cc7f51b4c9e8c828aec0e72648f8e6178e2ec0
Vidhi Agarwal <46235322+vidhi-agarwal@users.noreply.github.com>
2020-10-21 00:07:49 +0530

    improved happybirthday.py

--- examples/happybirthday.py
+++ examples/happybirthday.py
@@ -1,19 +1,18 @@
-j=int(input())
-n=j-1
-y=n
+c=int(input("Enter your age: "))
+n=c-1
 u=1
 i=0
-while i<j:
- i+=1
- for z in range (n):
-  print('  ',end="")
- for q in range (u):
-  print('||',end="")
- print ("")
- u+=2
- n-=1
-for k in range (y):
- print ('  ',end="")
+while i<c:
+          i+=1
+          for line in range (n):
+                    print('  ',end="")
+          for lines in range (u):
+                    print('||',end="")
+          print ("")
+          u+=2
+          n-=1
+for k in range (n):
+          print ('  ',end="")
 print ("")
-print ("HAPPY BIRTHDAY!")
+print ("HAPPY"+' '+str(c)+' '+"TH"+' '+ "BIRTHDAY!")
  
