1f069429fd5f5650db0e66e0eec294d769da4136
Vidhi Agarwal <46235322+vidhi-agarwal@users.noreply.github.com>
2020-10-20 23:45:38 +0530

    Update happybirthday.py

--- examples/happybirthday.py
+++ examples/happybirthday.py
@@ -3,7 +3,6 @@ n=j-1
 y=n
 u=1
 i=0
-
 while i<j:
  i+=1
  for z in range (n):
@@ -13,10 +12,8 @@ while i<j:
  print ("")
  u+=2
  n-=1
- 
 for k in range (y):
  print ('  ',end="")
 print ("")
-
 print ("HAPPY BIRTHDAY!")
  
