665f4d53dc36f10268e7edd7dc45359120fe4716
Apoorva <apoorva.me1@gmail.com>
2021-10-01 08:38:26 +0530

    Added happy_birthday java file for hacktoberfest

---
+++ examples/happy_birthday.java
@@ -0,0 +1,16 @@
+/* 
+@author apoorva1823000
+*/
+
+import java.util.Scanner;
+
+public class happy_birthday {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Enter your name:-");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Happy Birthday "+name);
+    }
+}
