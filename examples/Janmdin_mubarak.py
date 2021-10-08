f098499c9458c5d83d643575d2eb518d960889e5
m3ta_c1ph4r <ashutoshg547@gmail.com>
2021-10-08 17:54:27 +0530

    code_snippet : python .  I have added some messages which usually friends says.

---
+++ examples/Janmdin_mubarak.py
@@ -0,0 +1,7 @@
+import random
+
+#Message Contains those messages which usually friends say
+message = ["Happy Birthday Bete🍷🍷","Happy Birthday Bro","Happy Birthday ladke","Happy birthday asshole","Happy Birthday Bitch !!"]
+
+n=random.randint(0,len(message)-1)
+print(message[n])
