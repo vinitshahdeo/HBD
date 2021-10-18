2a4cedf7c7c9e0221568d976185814356f2f63f9
Coder TJ <83976571+TJdev-codes@users.noreply.github.com>
2021-10-18 13:38:11 -0400

    Update surprise.js
    Switched to inline JS.

--- examples/surprise.js
+++ examples/surprise.js
@@ -1,17 +1,19 @@
-let emotionType = ["Happy", "Sad", "Angry", "Scared"]
-let celebrationType = ["Retirement!", "Birthday!", "First Day of School!", "Anniversary!"]
-const emotion = emotionType[0]
-const celebration = celebrationType[1]
+<!DOCTYPE html>
+<html>
+<body>
+<p id="card" onclick="greeting()">Click me for a surprise!</p>
+<script>
+  let emotionType = ["Happy", "Sad", "Angry", "Scared"]
+  let celebrationType = ["Retirement!", "Birthday!", "First Day of School!", "Anniversary!"]
+  const emotion = emotionType[0]
+  const celebration = celebrationType[1]
 
-function greeting() {
-  document.getElementById("card").innerHTML = (emotion + " " + celebration)
-}
+  function greeting() {
+    document.getElementById("card").innerHTML = (emotion + " " + celebration)
+  }
+</script>
+</body>
+</html> 
 
-/* Add this HTML to activate the surprise
-  <!DOCTYPE html>
-  <html>
-  <body>
-  <p id="card" onclick="greeting()">Click me for a surprise!</p>
-  </body>
-  </html> 
-*/
+
+
