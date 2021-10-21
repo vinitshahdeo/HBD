261b87926e84dd9a379fa45df3351402ffe7dae8
Tarannum Ara <44407876+giTan7@users.noreply.github.com>
2021-10-21 13:59:10 +0530

    Birthday Wishes Program in java added
    Birthday Wishes Program in java added

---
+++ examples/BirthdayProgram.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ * @author Tarannum Ara
+ *
+ */
+public class BirthdayProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("HHHH     HHHH       AA         PPPPPPPPPPP  PPPPPPPPPPP    YY         YY ");
+        System.out.println("HHHH     HHHH       AA         PPPPPPPPPPP  PPPPPPPPPPP    YY         YY ");
+        System.out.println("HHHH     HHHH      AAAA        PP       PP  PP       PP     YY       YY ");
+        System.out.println("HHHH     HHHH     AA  AA       PP       PP  PP       PP      YY     YY  ");
+        System.out.println("HHHHHHHHHHHHH    AA    AA      PPPPPPPPPPP  PPPPPPPPPPP       YYYYYYY ");
+        System.out.println("HHHH     HHHH   AAAAAAAAAA     PP           PP                    YY ");
+        System.out.println("HHHH     HHHH  AA        AA    PP           PP                   YY ");
+        System.out.println("HHHH     HHHH AA          AA   PP           PP                  YY ");
+
+        System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RRRRRRRRRRRR  TTTTTTTTTTTTTT HHHH       HHHH  DDDDDDDDDDD        AA   YY          YY ");
+        System.out.println("BB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D      AAAA   YY        YY ");
+        System.out.println("BB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D    AA  AA   YY      YY ");
+        System.out.println("BBBBBBBBBB        II       RRRRRRRRRRR         TT       HHHHHHHHHHHHHHH  DD          D   AA    AA   YYYYYYYY ");
+        System.out.println("BB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D  AAAAAAAAAA       YY ");
+        System.out.println("BB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D  AA        AA     YY  ");
+        System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RR         RR       TT       HHHH       HHHH  DDDDDDDDDDD  AA          AA   YY ");
+
+        System.out.println("                       ~~");
+        System.out.println("                       ||");
+        System.out.println("                       ||");
+        System.out.println("                 ~~~~~~~~~~~~~~~~~   ");
+        System.out.println("                |                 |");
+        System.out.println("                |                 |");
+        System.out.println("            ____|                 |___");
+        System.out.println("           |     `````````````````    |");
+        System.out.println("           |     Congratulations!     |");
+        System.out.println("     ______|                          |_____   ");
+        System.out.println("     |      ```````````````````````````     | ");
+        System.out.println("     |       	 Happy Birthday!");
+        System.out.println("     |______________________________________|    ");
+    }
+    
+}
