a282d6ed8bce478d6faeca8684eed987dcea76ae
vinayakchachra <Imvinayak1@gmail.com>
2022-10-13 18:19:45 +0530

    contribute to return happy birthday bro
     contribute to return happy birthday bro

---
+++ examples/HappyBirthday.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void happy(){
+    cout<<"happy ";
+}
+
+void birthday(){
+    cout<<"birthday ";
+}
+
+void bro(){
+    cout<<"bro";
+}
+int main()
+{
+   happy();
+   birthday();
+   bro();
+   return 0;
+}
